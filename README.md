a3c774ca111113c89203a879ddc0e3a490fb305c
PragadaBharathRaj <bharathrajpragada@gmail.com>
2024-04-10 22:44:55 -0400

    Predictive Modeling for Online Shopping using clickstream data

--- README.md
+++ README.md
@@ -1,2 +1,27 @@
 # Predictive_Modeling_of_Clickstream_data_for_Online_Shopping
 The goal is to analyze patterns within the clickstream data and predict the potential purchasing amount or price for each user. The goal is to analyze patterns within the clickstream data and label each user interaction based on the potential purchase they might make in that ongoing session
+
+
+Project Phases:
+Phase 1: Dataset Selection and Preprocessing
+● Select a dataset suitable for supervised learning tasks.
+● Perform data cleaning, normalization, and splitting into training and testing sets.
+Phase 2: Exploratory Data Analysis (EDA) and Feature Selection
+● Conduct EDA to understand the dataset's characteristics.
+● Apply feature selection techniques to identify relevant features for modeling.
+Phase 3: Model Implementation and Baseline Evaluation
+● Implement at least three supervised learning algorithms (e.g., SVM, Decision Trees,
+Neural Networks).
+● Evaluate baseline models using initial features without hyperparameter tuning.
+Phase 4: Hyperparameter Tuning
+● Use techniques like grid search or random search to find optimal hyperparameters for
+each model.
+● Re-evaluate models with tuned hyperparameters to assess performance improvements.
+Phase 5: Model Evaluation and Comparative Analysis
+● Evaluate models using metrics such as accuracy, precision, recall, F1 score, and
+ROC-AUC.
+● Compare the performance, computational efficiency, and applicability of each algorithm.
+Phase 6: Conclusion and Recommendations
+● Summarize findings and insights gained from the comparative analysis.
+● Provide recommendations on the most suitable algorithms for the dataset and problem
+type.
---
+++ app.py
@@ -0,0 +1,58 @@
+from flask import Flask, render_template, request, jsonify
+import joblib
+
+
+app = Flask(__name__)
+
+# Load your regression and classification models
+regression_model = joblib.load('models/regressor_model.joblib')
+classification_model = joblib.load('models/classification_model.joblib')
+
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    if request.method == 'POST':
+        model_type = request.form['model_type']
+        if model_type == 'regression':
+            return render_template('input_reg.html')
+        elif model_type == 'classification':
+            return render_template('input_cla.html')
+    return render_template('index.html')
+
+@app.route('/predict_regression', methods=['POST'])
+def predict_regression():
+    # Get input values from the form
+    # Assuming input names are 'feature1', 'feature2', etc.
+    feature1 = int(request.form['Month'])
+    feature2 = int(request.form['Category'])
+    feature3 = int(request.form['Colour'])
+    feature4 = int(request.form['Picture Location'])
+    feature5 = int(request.form['Model-Photography'])
+    feature6 = int(request.form['Product Code'])
+
+
+    # Add more features as needed
+
+    # Make prediction using the regression model
+    prediction = regression_model.predict([[feature1,feature2,feature3,feature4,feature5,feature6]])
+    return jsonify({'prediction': prediction[0]})
+
+@app.route('/predict_classification', methods=['POST'])
+def predict_classification():
+    # Get input values from the form
+    # Assuming input names are 'feature1', 'feature2', etc.
+    feature1 = int(request.form['Month'])
+    feature2 = int(request.form['Category'])
+    feature3 = int(request.form['Colour'])
+    feature4 = int(request.form['Picture Location'])
+    feature5 = int(request.form['Model-Photography'])
+    feature6 = int(request.form['Product Code'])
+
+    # Add more features as needed
+
+    # Make prediction using the classification model
+    prediction = classification_model.predict([[feature1, feature2,feature3,feature4,feature5,feature6]])  # Adjust based on your features
+    # print(prediction)
+    return jsonify({'prediction': int(prediction[0])})
+
+if __name__ == '__main__':
+    app.run(debug=True)
